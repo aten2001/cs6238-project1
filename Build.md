f7030c7bf5e88384d2b9fa774442f0284bf5abb0
Kyle Koza <kyle.koza@gatech.edu>
2015-10-04 18:52:54 -0400

    Add some crypto functionality to HistoryFile Work is still needed

--- Build.md
+++ Build.md
@@ -1,3 +1,4 @@
 # Build instructions
 ## Python Modules
 pycrypto
+cryptography
--- HistoryFile.py
+++ HistoryFile.py
@@ -1,6 +1,14 @@
+import base64
+import os
 import shelve
 import Config
+import cryptography
 from collections import deque
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import padding
 
 BASE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                          "data")
@@ -9,18 +17,31 @@ config = Config.getConfig()
 
 class HistoryFile(object):
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, salt):
         self.username = username
         self.password = password
+        self.token = tokenFromPassword(self)
+        self.salt = salt
         self.history = self.getHistory()
     
-    def encrypt(self, featurearray):
-        # TODO: encrypt with password
-        return featurearray
+    def tokenFromPassword(self):
+        kdf = PBKDF2HMAC(
+                algorithm=hashes.SHA256(),
+                length=32,
+                salt=self.salt,
+                iterations=100000,
+                backend=default_backend()
+              )
+        token = base64.urlsafe_b64encode(kdf.derive(self.password))
+        return token
 
-    def decrypt(self, featurearray):
+    def encrypt(self):
+        # TODO: encrypt with password
+        pass
+
+    def decrypt(self):
         # TODO: decrypt with password
-        return featurearray
+        pass
 
     def getHistory(self):
         shelf = shelve.open(os.path.join(BASE_PATH, self.username))
@@ -47,3 +68,21 @@ class HistoryFile(object):
                     temp = queue.popleft()
                 history.append(self.encrypt(featureArray))
                 shelf["history"] = history
+
+    def open(self):
+        # Read in file
+        with open(os.path.join(BASE_PATH, self.username)) as f:
+            # Decrypt file w/ password
+            fern = Fernet(self.token)
+            cipher = f.read()
+            plaintext = fern.decrypt(cipher)
+            # TODO: Unpad file
+            # Unpickle array
+            # Return array
+
+    def write(self, array):
+        # Pickle array
+        # Pad array
+        # Encrypt array w/ password
+        # Write to file
+        pass
