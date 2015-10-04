9127ae33918454f3e393f17c786bc4537bad3956
Kyle Koza <kyle.koza@gatech.edu>
2015-10-04 14:51:39 -0400

    Add ability to set configuration

--- User.py
+++ User.py
@@ -1,10 +1,13 @@
 import Crypto.Util.number
 import random
+import Config
+
+config = Config.getConfig()
 
 class User(object):
 
     def __init__(self, username):
         self.username = username
         self.q = Crypto.Util.number.getPrime(160)
-        self.h = 4
+        self.h = config.get("historyfile", "length")
         self.hpwd = random.randint(1, self.q)
