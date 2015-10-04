ee796cf722486068e135a2f18dc23a563f23c9f9
Kyle Koza <kyle.koza@gatech.edu>
2015-10-04 14:17:11 -0400

    Add various history file methods to User

--- User.py
+++ User.py
@@ -1,10 +1,22 @@
 import Crypto.Util.number
 import random
+import os
+import HistoryFile
+
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 class User(object):
 
-    def __init__(self, username):
+    def __init__(self, username, password):
         self.username = username
         self.q = Crypto.Util.number.getPrime(160)
-        self.h = 4
+        self.h = 5
         self.hpwd = random.randint(1, self.q)
+        self.historyfile = self.getHistoryFile()
+        self.passwd = password
+
+    def getHistoryFile(self):
+        return HistoryFile(self.username, self.password)
+
+    def saveHistFile(self):
+        pass
