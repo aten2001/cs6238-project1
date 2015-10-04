f6714d1ba003a432fd79010b678923fbd2f01cca
Kyle Koza <kyle.koza@gatech.edu>
2015-10-04 16:14:11 -0400

    Remove history file length from User since it is in config

--- User.py
+++ User.py
@@ -13,7 +13,6 @@ class User(object):
     def __init__(self, username, password):
         self.username = username
         self.q = Crypto.Util.number.getPrime(160)
-        self.h = config.get("historyfile", "length")
         self.hpwd = random.randint(1, self.q)
         self.historyfile = self.getHistoryFile()
         self.passwd = password
