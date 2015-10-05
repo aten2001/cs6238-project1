a1251e52f218c6207a9fb69dbaf7e80d899679c1
Kyle Koza <kyle.koza@gatech.edu>
2015-10-05 19:11:40 -0400

    Begin instruction table generation

---
+++ InstructionTable.py
@@ -0,0 +1,27 @@
+import Config
+
+config = Config.getConfig()
+
+class InstructionTable(object):
+
+    def __init__(self, coefficients, q):
+        self.q = q
+        self.coefficients = coefficients
+
+    def generateTable(self):
+        degree = config.get("general", "features")
+        table = []
+        # TODO: add in G
+        for i in range(1, degree + 1):
+            table.append([i, 
+                          self.polynomial(self.coefficients, 2 * i),
+                          self.polynomial(self.coefficients, 2 * i + 1)])
+        return table
+
+    def polynomial(self, base):
+        result = 0
+        exponent = 0
+        for coefficient in self.coefficients:
+            result += coefficient * pow(base, exponent)
+            exponent += 1
+        return result
--- User.py
+++ User.py
@@ -13,13 +13,21 @@ class User(object):
     def __init__(self, username, password):
         self.username = username
         self.q = Crypto.Util.number.getPrime(160)
-        self.hpwd = Crypto.Random.random.randint(1, self.q) # TODO: don't use random
+        self.hpwd = Crypto.Random.random.randint(1, self.q)
         self.salt = os.urandom(16)
         self.password = password # TODO: hash this
         self.historyfile = self.getHistoryFile()
+        self.polynomial = self.genPolynomial()
 
     def getHistoryFile(self):
         return HistoryFile.HistoryFile(self.username, self.hpwd, self.salt)
 
     def saveHistFile(self):
         pass
+
+    def genPolynomial(self):
+        degree = config.get("general", "features") - 1
+        a = [self.hpwd]
+        for i in range(0, degree):
+            a.append(Crypto.Random.random.randint(1, self.q))
+        return a
