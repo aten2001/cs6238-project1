2461887c6212bda12b726dc107b0dfcae85ef51e
Kyle Koza <kyle.koza@gatech.edu>
2015-10-05 14:02:08 -0400

    User Crypto.Random instead of random

--- User.py
+++ User.py
@@ -1,5 +1,5 @@
 import Crypto.Util.number
-import random
+import Crypto.Random.random
 import os
 import HistoryFile
 import Config
@@ -13,13 +13,13 @@ class User(object):
     def __init__(self, username, password):
         self.username = username
         self.q = Crypto.Util.number.getPrime(160)
-        self.hpwd = random.randint(1, self.q)
+        self.hpwd = Crypto.Random.random.randint(1, self.q) # TODO: don't use random
         self.salt = os.urandom(16)
-        self.password = password
+        self.password = password # TODO: hash this
         self.historyfile = self.getHistoryFile()
 
     def getHistoryFile(self):
-        return HistoryFile.HistoryFile(self.username, self.password, self.salt)
+        return HistoryFile.HistoryFile(self.username, self.hpwd, self.salt)
 
     def saveHistFile(self):
         pass
