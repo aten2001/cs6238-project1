69665bf2947dfc63afc42e364c3b939212c5cfa1
Kyle Koza <kyle.koza@gatech.edu>
2015-10-09 15:58:04 -0400

    Generate alpha and beta

--- InstructionTable.py
+++ InstructionTable.py
@@ -1,21 +1,28 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, hmac
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import Config
+import os
 
 config = Config.getConfig()
 
 class InstructionTable(object):
 
-    def __init__(self, coefficients, q):
+    def __init__(self, coefficients, q, password):
         self.q = q
         self.coefficients = coefficients
+        self.password = password
+        self.salt = os.urandom(16) 
 
     def generateTable(self):
         degree = config.get("general", "features")
         table = []
-        # TODO: add in G
         for i in range(1, degree + 1):
+            alpha = getAlpha(i)
+            beta = getBeta(i)
             table.append([i,
-                          self.polynomial(self.coefficients, 2 * i),
-                          self.polynomial(self.coefficients, 2 * i + 1)])
+                          alpha,
+                          beta])
         return table
 
     def polynomial(self, base):
@@ -25,3 +32,35 @@ class InstructionTable(object):
             result += coefficient * pow(base, exponent)
             exponent += 1
         return result
+
+    def genAlpha(self, i):
+        backend = default_backend()
+        kdf = PBKDF2HMAC(
+                algorithm=hashes.SHA256(),
+                length=32,
+                salt=self.salt,
+                iterations=100000,
+                backend=backend
+              )
+
+        y = self.polynomial(self.coefficients, 2 * i)
+
+        G = long(kdf.derive('hex'),16)
+        alpha = y + G % self.q
+        return alpha
+
+    def genBeta(self, i):
+        backend = default_backend()
+        kdf = PBKDF2HMAC(
+                algorithm=hashes.SHA256(),
+                length=32,
+                salt=self.salt,
+                iterations=100000,
+                backend=backend
+              )
+
+        y = self.polynomial(self.coefficients, 2 * i + 1)
+
+        G = long(kdf.derive('hex'),16)
+        beta = y + G % self.q
+        return beta
