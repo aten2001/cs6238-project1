31b204e37d1794195c609a6f684889b52675fac0
Anant Lummis <jlummis@Anants-MBP.hsd1.ga.comcast.net>
2015-10-18 07:27:09 -0400

    Commented HistoryFile.py

--- HistoryFile.py
+++ HistoryFile.py
@@ -1,3 +1,14 @@
+"""
+CS6238 - Secure Computer Systems
+Project Team: Kyle Koza and Anant Lummis
+
+Object Name: HistoryFile.py
+Object Functions: tokenFromPassword, addEntry, encrypt, decrypt
+Object Description:  The purpose of this object is to instantiate and manage a history containing a set of previous instruction tables.
+                     The purpose of the history file is to allow for the final authentication check using the Hpwd as well as to calculate mean and std. deviation of
+                     typing features.
+"""
+
 import base64
 import os
 import pickle
@@ -18,7 +29,8 @@ BASE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),
 config = Config.getConfig()
 
 class HistoryFile(object):
-
+    
+    #Object initialization function.  Takes in the hardened password (Hpwd) and a hash function salt as inputs and instantiates the History file
     def __init__(self, password, salt, history=None):
         self.password = password
         self.salt = salt
@@ -32,7 +44,8 @@ class HistoryFile(object):
             except cryptography.fernet.InvalidToken:
                 # TODO: raise error
                 raise PasswordError("Incorrect Password")
-
+    
+    #Function tokenFromPassword(), hashes the user password using the provided salt and the SHA256 hash function            
     def tokenFromPassword(self):
         kdf = PBKDF2HMAC(
                 algorithm=hashes.SHA256(),
@@ -44,21 +57,24 @@ class HistoryFile(object):
         token = base64.urlsafe_b64encode(
                   kdf.derive(helpers.long_to_bytes(self.password)))
         return token
-
+        
+    #Function addEntry(featureArray), accepts an instruction table array and appends it to the existing history file
     def addEntry(self, featureArray):
         length = config.get("historyfile", "length")
-        if len(self.history) >= length:
+        if len(self.history) >= length:     #If the history file is at maximum capacity, remove the oldest entry 
             for i in range(0, (len(self.history) - length) + 1):
                 temp = self.history.popleft()
         self.history.append(featureArray)
 
+    #Function encrypt(), takes the history file and encrypts the file using the Fernet symmetric key encryption algorithm and the token generated from Hpwd
     def encrypt(self):
         pickledQueue = pickle.dumps(self.history)
         # TODO: pad queue
         fern = Fernet(self.token)
         cipher = fern.encrypt(pickledQueue)
         return cipher
-
+    
+    #Function decrypt(), takes an encrypted history file and decrypts the file using the token generated from Hpwd
     def decrypt(self, history):
         fern = Fernet(self.token)
         plaintext = fern.decrypt(history)
